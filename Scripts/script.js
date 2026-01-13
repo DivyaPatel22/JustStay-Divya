eb979fed54d5c30db3768c3774205356568b5aeb
DivyaPatel22 <dpatel.101122@gmail.com>
2026-01-13 07:30:58 +0530

    Add files via upload

---
+++ Scripts/script.js
@@ -0,0 +1,10 @@
+function closesidebar(){
+    const sidebar=document.querySelector(".sidebar")
+    sidebar.style.display="none";
+
+}
+
+function opensidebar(){
+    const sidebar=document.querySelector(".sidebar")
+    sidebar.style.display="flex";
+}
